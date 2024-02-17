8ef6d7dcc154edc34386174cad8a8f803ed01eff
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-02-17 15:45:49 -0800

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { SignInButton } from "@clerk/clerk-react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { SignInButton, SignOutButton } from "@clerk/clerk-react";
 import {
   AuthLoading,
   Authenticated,
@@ -10,18 +11,82 @@ import { useUser } from "@clerk/clerk-react";
 export default function HomePage() {
   const { isAuthenticated } = useConvexAuth();
   const { user } = useUser();
-  console.log(user)
-  
-  return (
-    <div className="">
-      {isAuthenticated ? "Logged in" : "Logged out or still loading"}
-      <Authenticated>Logged in</Authenticated>
-      <Unauthenticated>Logged out</Unauthenticated>
-      <AuthLoading>Still loading</AuthLoading>
-      return <span>Logged in as {user?.fullName}</span>;
+  console.log(user);
 
-      
-      <SignInButton mode="modal" />
+  return (
+    <div className="w-full ">
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="w-full navbar bg-secondary">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="flex-1 px-2 mx-2 font-bold text-2xl text-white">
+              Prettier Notes
+            </div>
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                <li>
+                  {isAuthenticated ? (
+                    <button className="btn btn-primary">
+                      <SignOutButton />
+                    </button>
+                  ) : (
+                    <button className="btn btn-primary">
+                      <SignInButton mode="modal" />
+                    </button>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* Page content here */}
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {isAuthenticated ? (
+        <h1 className="text-xl font-bold">Welcome {user?.fullName}</h1>
+      ) : (
+        <h1 className="text-xl font-bold">Welcome to the home page</h1>
+      )}
+
     </div>
   );
 }
