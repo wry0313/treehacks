6ca24bce8fbc0d5fabdc754e8bda0354b23ac85f
Rudy Arora <rudy.arora.uni@gmail.com>
2024-02-18 00:55:36 -0800

    added pdf

--- src/pages/note/[id].tsx
+++ src/pages/note/[id].tsx
@@ -11,12 +11,29 @@ export default function NotePage() {
   const [mode, setMode] = useState<"Upload" | "Feedback" | "Chatbot">("Upload");
   return (
     <div className="flex flex-row h-full w-full grow">
-      <div className="flex w-[25%] bg-blue-500 h-[100vh]">
+      <div className="flex w-[15%] bg-blue-500 h-[100vh]">
         <a
           href="/"
           className="hover:underline text-white font-semibold fixed top-1 left-2"
         >
-          Back
+
+          <a href="/" className="text-white font-semibold fixed top-1 left-2 mt-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 hover:opacity-75"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </a>
+
         </a>
         <div className="flex flex-col items-center  w-full text-white text-2xl mt-20 gap-y-10">
           <button
@@ -39,9 +56,11 @@ export default function NotePage() {
           </button>
         </div>
       </div>
+
+
       {notes && (
-        <div className="flex flex-col">
-          <p className="text-2xl font-semibold p-2 h-[50px]">{notes?.title}</p>
+        <div className="flex flex-col pl-10">
+          <p className="text-2xl font-semibold p-2 h-[50px] bg-gray-200"> {notes?.title}  </p>
           <div>
             {mode === "Upload" && (
               <div className="flex flex-col">
@@ -63,6 +82,87 @@ export default function NotePage() {
                       ))}
                   </div>
                 </div>
+              </div>
+            )}
+
+            
+
+            {mode === "Upload" && (
+              <div className="flex flex-col">
+                <UploadNotesImagePage noteId={notes._id} />
+                <div className="p-4 w-[600px]">
+                  <p className="font-semibold pb-2">
+                    Images uploaded for the notes:
+                  </p>
+                  <div className="flex flex-row gap-x-2">
+                    {images &&
+                      images.map((image, index) => (
+                        <img
+                          key={index}
+                          src={image as string}
+                          alt="Note Image"
+                          height={300}
+                          width={100}
+                        />
+                      ))}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {mode === "Upload" && (
+              <div className="flex flex-col">
+                <UploadNotesImagePage noteId={notes._id} />
+                <div className="p-4 w-[600px]">
+                  <p className="font-semibold pb-2">
+                    Images uploaded for the notes:
+                  </p>
+                  <div className="flex flex-row gap-x-2">
+                    {images &&
+                      images.map((image, index) => (
+                        <img
+                          key={index}
+                          src={image as string}
+                          alt="Note Image"
+                          height={300}
+                          width={100}
+                        />
+                      ))}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {mode === "Upload" && (
+              <div className="flex flex-col">
+                <UploadNotesImagePage noteId={notes._id} />
+                <div className="p-4 w-[600px]">
+                  <p className="font-semibold pb-2">
+                    Images uploaded for the notes:
+                  </p>
+                  <div className="flex flex-row gap-x-2">
+                    {images &&
+                      images.map((image, index) => (
+                        <img
+                          key={index}
+                          src={image as string}
+                          alt="Note Image"
+                          height={300}
+                          width={100}
+                        />
+                      ))}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {mode === "Feedback" && (
+              <div className="w-[40vw] h-[100vh]">
+                <iframe
+                  src="https://arxiv.org/pdf/2402.09859.pdf"
+                  width="100%"
+                  height="100%"
+                ></iframe>
               </div>
             )}
           </div>
